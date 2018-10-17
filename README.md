91a699624e2f3bcd68bc0716ebaecb023ddf792d
yhui <yanghui.me@outlook.com>
2018-10-17 20:55:06 +0800

    add readme

---
+++ README.md
@@ -0,0 +1,5 @@
+**本项目fork自https://github.com/diaozxin007/xilidou-framework，要了解详细步骤的话，请看作者博文[徒手撸框架--实现IoC](https://diaozxin007.github.io/2018/01/08/spring-ioc/)**
+
+##### 在原项目基础上，略加改造。借这个项目，深入的了解了Spring IoC的思想，也算是对平时工作和学习的一个总结。
+
+1.  实现了XML作为配置文件的容器
--- src/main/java/com/zhixilang/framework/ioc/Application.java
+++ src/main/java/com/zhixilang/framework/ioc/Application.java
@@ -1,8 +1,5 @@
 package com.zhixilang.framework.ioc;
 
-
-import org.apache.commons.lang3.StringUtils;
-
 /**
  * @author yhui
  * @date 2018-10-16 17:38
@@ -11,11 +8,5 @@ public class Application {
     public static void main(String[] args) {
 
         // TODO 基于annotation
-        // http://heeexy.com/2018/01/28/IoC/
-
-
-        String name = "hello";
-        System.out.println(StringUtils.capitalize(name));
-
     }
 }
